6915c6e23a1e531eca40d2b5e7334565c1a3a13d
savosburn <savosburn18@tampabay.rr.com>
2021-06-03 21:06:17 -0400

    took a while but finally a commit

--- src/main/java/ex05/App.java
+++ src/main/java/ex05/App.java
@@ -5,8 +5,62 @@
 
 package ex05;
 
-public class App {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App myApp = new App();
+
+        String numOne = myApp.stringOne();
+        String numTwo = myApp.stringTwo();
+
+        int newNumOne = Integer.parseInt(numOne);
+        int newNumTwo = Integer.parseInt(numTwo);
+
+        int add = myApp.addition(newNumOne, newNumTwo);
+        int subtract = myApp.subtraction(newNumOne, newNumTwo);
+        int multiply = myApp.multiplication(newNumOne, newNumTwo);
+        int divide = myApp.division(newNumOne, newNumTwo);
+
+        String generateOutput = myApp.concatenation(numOne, numTwo, add, subtract, multiply, divide);
+
+        myApp.output(generateOutput);
+    }
+
+    public String stringOne() {
+        System.out.print("What is your first number? ");
+        return in.nextLine();
+    }
+
+    public String stringTwo() {
+        System.out.print("What is your second number? ");
+        return in.nextLine();
+    }
+
+    public int addition(int newNumOne, int newNumTwo) {
+        return newNumOne + newNumTwo;
+    }
+
+    public int subtraction(int newNumOne, int newNumTwo) {
+        return newNumOne - newNumTwo;
+    }
+
+    public int multiplication(int newNumOne, int newNumTwo) {
+        return newNumOne * newNumTwo;
+    }
+
+    public int division(int newNumOne, int newNumTwo) {
+        return newNumOne / newNumTwo;
+    }
+
+    public String concatenation(String newNumOne, String newNumTwo, int add, int subtract, int multiply, int divide) {
+        return String.format("%s + %s = %d\n%s - %s = %d\n%s * %s = %d\n%s / %s = %d", newNumOne, newNumTwo, add,
+                newNumOne, newNumTwo, subtract, newNumOne, newNumTwo, multiply, newNumOne, newNumTwo, divide );
+    }
+
+    public void output(String outputGenerator) {
+        System.out.print(outputGenerator);
     }
 }
---
+++ src/test/java/ex05/AppTest.java
@@ -0,0 +1,31 @@
+package ex05;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    public void output_includes_strings_and_calculations() {
+        App myApp = new App();
+
+        String stringOne = "10";
+        String stringTwo = "5";
+
+        int add = 15;
+        int subtract = 5;
+        int multiply = 50;
+        int divide = 2;
+
+        String expectedOutput = """
+                10 + 5 = 15
+                10 - 5 = 5
+                10 * 5 = 50
+                10 / 5 = 2""";
+
+        String actualOutput = myApp.concatenation(stringOne, stringTwo, add, subtract, multiply, divide);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
